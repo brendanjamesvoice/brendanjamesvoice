6db071172da7a0e571cad3d5faf042db097ba511
Michael Joyce <michael.a.joyce@gmail.com>
2020-12-05 17:03:31 -0300

    Version 1 of brendanjamesvoice.com

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,39 @@ import { TwitterVideoEmbed } from "react-twitter-embed";
 import Image from "next/image";
 import styles from "../styles/styles.module.sass";
 
+function ClipContainer({ title = "", src = "" }) {
+  return (
+    <div className={styles.clip}>
+      <div className={styles.clipHeader}>{title}</div>
+      <div className={styles.clipFrame}>
+        <iframe
+          src={src}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
-          <h1 className={styles.title}>
+          <h1 className={styles.headerTitle}>
             Brendan
             <br />
-            James<span>&nbsp;Voice</span>
+            James
+            <span>Voice</span>
           </h1>
           <Image
+            className={styles.headerImage}
             src="/headshot.png"
             alt="Brendan James"
             width={200}
-            height={187}
+            height={200}
           />
         </div>
       </header>
@@ -25,132 +43,104 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.hero}>
           <section className={styles.aboutSection}>
-            <h2 className={styles.sectionHeader}>About</h2>
-            <p class={styles.aboutCopy}>
-              With over ten years of experience, Brendan James has been the
-              voice of multiple national and regional ad campaigns for radio and
-              television as well as professional sporting events and countless
-              corporate industrial and instructional videos. He has worked with{" "}
+            <p>
+              Brendan James is the voice of ad campaigns for internationally
+              recognized brands including{" "}
               <strong>
-                Burger King, Audi, Kawasaki, Major League Soccer, Comcast
-              </strong>
-              , and many more. He has a professional home studio with full
-              recording capabilities and can quickly deliver edited VO tracks.
+                Burger King, Budweiser, Audi, Kawasaki, Comcast, Nestle
+              </strong>{" "}
+              and others. His decade-plus career features multiple seasons of
+              promos for <strong>Major League Soccer</strong> as well as an
+              eclectic portfolio including videos in the technology and gaming
+              industries.
+            </p>
+            <p>
+              Brendan produces edited voice-over tracks out of his professional
+              home studio in upstate New York. To book his services for your
+              next campaign, contact him at{" "}
+              <a href="mailto:brendanjamesvo@gmail.com">
+                brendanjamesvo@gmail.com
+              </a>
+              .
             </p>
           </section>
-          <section className={styles.bookingContactSection}>
-            <h2 className={styles.sectionHeader}>Booking Contacts</h2>
-            <div className={styles.bookingContactChips}>
-              <div className={styles.bookingContactChip}>
-                <strong>Allan Duncan</strong>
-                <br />
-                Innovative Artists
-                <br />
-                212-659-5126
-                <br />
-                allan.duncan@iany.com
-              </div>
-              <div className={styles.bookingContactChip}>
-                <strong>Eileen Schellhorn</strong>
-                <br />
-                DDO Artists
-                <br />
-                212-379-6314
-                <br />
-                eileen@ddonyc.com
-              </div>
+          <section className={styles.contactSection}>
+            <div className={styles.contact}>
+              <strong>Brendan James</strong>
+              <br />
+              845-270-9401
+              <br />
+              brendanjamesvo@gmail.com
+            </div>
+            <div style={{ marginTop: 40 }}>
+              <ClipContainer
+                title="Burger King | Delay Your Way"
+                src="https://www.youtube.com/embed/-qyJyvcVcVM"
+              />
             </div>
           </section>
         </div>
 
         <section className={styles.showcaseSection}>
-          <h2 className={styles.sectionHeader}>TV • Radio • Internet</h2>
           <div className={styles.grid}>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>
-                Burger King | Delay Your Way
+            <ClipContainer
+              title="Kawasaki | We’re Back"
+              src="https://www.youtube.com/embed/xmjfQEr4MCs"
+            />
+            <ClipContainer
+              title="Audi | Winter Sports"
+              src="https://www.youtube.com/embed/CN4MdqjjcWE"
+            />
+            <ClipContainer
+              title="MLS | Stand As One"
+              src="https://www.youtube.com/embed/w-lOJDXt7ag"
+            />
+            <ClipContainer
+              title="Optimum"
+              src="https://player.vimeo.com/video/117604935?title=0&byline=0&portrait=0"
+            />
+            <ClipContainer
+              title="Kyocera"
+              src="https://www.youtube.com/embed/DR5hprucYX0"
+            />
+            <ClipContainer
+              title="EVE Online"
+              src="https://www.youtube.com/embed/WG9W8e1CFGw"
+            />
+            <ClipContainer
+              title="MLS | 2015 Cup Playoffs"
+              src="https://www.youtube.com/embed/sp8BnX_HK5E"
+            />
+            <ClipContainer
+              title="Nestle | Pure Life"
+              src="https://www.ispot.tv/share/nuYn?start=15"
+            />
+            <ClipContainer
+              title="EVE Online"
+              src="https://www.youtube.com/embed/cYCNrTpsHLU"
+            />
+            <ClipContainer
+              title="Comcast | Rico Roman: Medals"
+              src="https://www.youtube.com/embed/fKw1jloEnrM?start=51"
+            />
+            <div>
+              <div className={styles.clipHeader}>
+                Budweiser | This Bud's For Dad
               </div>
-              <iframe
-                src="https://www.youtube.com/embed/-qyJyvcVcVM"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={`${styles.gridCell} ${styles.tweetCell}`}>
               <TwitterVideoEmbed id={"1008331138585251840"} />
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>AudiBlog.nl Presents</div>
-              <iframe
-                src="https://www.youtube.com/embed/CN4MdqjjcWE"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>Optimum</div>
-              <iframe
-                src="https://player.vimeo.com/video/117604935?title=0&byline=0&portrait=0"
-                frameborder="0"
-                allow="autoplay; fullscreen"
-                allowfullscreen
-                style={{
-                  transform: "translateX(-10px)",
-                }}
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>MLS | Stand As One</div>
-              <iframe
-                src="https://www.youtube.com/embed/w-lOJDXt7ag"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>Kyocera</div>
-              <iframe
-                src="https://www.youtube.com/embed/DR5hprucYX0"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>EVE Online</div>
-              <iframe
-                src="https://www.youtube.com/embed/WG9W8e1CFGw"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>
-                MLS | 2015 Cup Playoffs
-              </div>
-              <iframe
-                src="https://www.youtube.com/embed/sp8BnX_HK5E"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>Commercials 1</div>
+            </div>
+            <div>
+              <div className={styles.clipHeader}>MP3 | Commercials 1</div>
               <audio controls src="/commercials.mp3" />
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>Commercials 2</div>
+              <div className={styles.clipHeader} style={{ marginTop: 20 }}>
+                MP3 | Commercials 2
+              </div>
               <audio controls src="/commercials2.mp3" />
-            </article>
-            <article className={styles.gridCell}>
-              <div className={styles.gridCellHeader}>Promos</div>
+              <div className={styles.clipHeader} style={{ marginTop: 20 }}>
+                MP3 | Promos
+              </div>
               <audio controls src="/promo.mp3" />
-            </article>
+            </div>
           </div>
         </section>
       </main>
